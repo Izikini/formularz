54b539b80663f72c8da1fe232c6dfeaad9ca34b2
Bohdan Bura <butashka228@gmail.com>
2026-01-06 17:16:46 +0100

    dont know whats wrong

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-<<<<<<< HEAD
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.jsx'
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> cad262baa44197f0f5701e3a263af76ce192300c
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
